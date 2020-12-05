90973534cafb5fd52d9bc7dd26765a557a89564e
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2020-12-05 10:41:09 +0800

    Accept input arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::result::Result<(), String> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        Err("expected exactly one arg".into())
+    } else {
+        let input_path = std::path::PathBuf::from(&args[1]);
+        println!("Got {:?}", input_path);
+        Ok(())
+    }
 }
