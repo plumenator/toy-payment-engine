8e73faadf1de29fc0e4e4463d9de136dbc2e0370
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2020-12-05 12:08:05 +0800

    Read tx records from csv

---
+++ src/csv.rs
@@ -0,0 +1,36 @@
+use std::io;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct TxRecord {
+    #[serde(alias = "type")]
+    tx_type: String,
+    client: u16,
+    tx: u32,
+    amount: String,
+}
+
+pub fn read_tx(reader: impl io::Read) -> impl Iterator<Item = Result<TxRecord, csv::Error>> {
+    let rdr = csv::Reader::from_reader(reader);
+    rdr.into_deserialize()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reads_tx() -> Result<(), csv::Error> {
+        let input = "type,client,tx,amount\n\
+                     deposit,1,1,1.0\n\
+                     deposit,2,2,2.0\n\
+                     deposit,1,3,2.0\n\
+                     withdrawal,1,4,1.5\n\
+                     withdrawal,2,5,3.0\n"
+            .as_bytes();
+
+        let result: Result<Vec<_>, _> = read_tx(input).collect();
+        result.map(|_| ())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-fn main() -> std::result::Result<(), String> {
+use std::io;
+
+use toy_payment_engine::csv::read_tx;
+
+fn main() -> io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
-        return Err("expected exactly one arg".into());
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            "expected exactly one arg",
+        ));
     }
     let input_path = std::path::Path::new(&args[1]);
-    if !input_path.exists() {
-        return Err("no such file".into());
+    let input_file = std::fs::File::open(input_path)?;
+    for tx_record in read_tx(input_file) {
+        println!("{:#?}", tx_record?);
     }
     Ok(())
 }
