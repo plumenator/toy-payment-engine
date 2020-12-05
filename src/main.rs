834b168dfd850deb2090309c29fb33801a9849f0
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2020-12-05 11:04:24 +0800

    Check that the input path exists

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 fn main() -> std::result::Result<(), String> {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
-        Err("expected exactly one arg".into())
-    } else {
-        let input_path = std::path::PathBuf::from(&args[1]);
-        println!("Got {:?}", input_path);
-        Ok(())
+        return Err("expected exactly one arg".into());
     }
+    let input_path = std::path::Path::new(&args[1]);
+    if !input_path.exists() {
+        return Err("no such file".into());
+    }
+    Ok(())
 }
